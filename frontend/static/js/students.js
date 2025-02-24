c8fe060af19d5e0d4453b1dfcc9be3cf051c5644
Sarah Nadi <nadi@ualberta.ca>
2025-02-24 16:02:31 +0400

    Handle early loading

--- frontend/static/js/students.js
+++ frontend/static/js/students.js
@@ -1,18 +1,24 @@
-
-// Add student
-document.getElementById('create-student-form').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    try {
-        const response = await axios.post(`${API_URL}/students/`, {
-            name: document.getElementById('name').value,
-            email: document.getElementById('email').value,
-            seniority: document.getElementById('seniority').value
-        });
-        alert('Student added successfully!');
-        e.target.reset();
-    } catch (error) {
-        alert('Error adding student: ' + error.response?.data?.message || error.message);
-    }
+document.addEventListener("DOMContentLoaded", () => {
+    // Add student
+    document.getElementById('create-student-form')?.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(`${API_URL}/students/`, {
+                name: document.getElementById('name').value,
+                email: document.getElementById('email').value,
+                seniority: document.getElementById('seniority').value
+            }, 
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            alert('Student added successfully!');
+            e.target.reset();
+        } catch (error) {
+            alert('Error adding student: ' + error.response?.data?.message || error.message);
+        }
+    });
 });
 
 // list students
